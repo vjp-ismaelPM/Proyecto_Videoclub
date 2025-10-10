9bdd75d4b02003ef42985d46883207452322a321
vjp-ismaelPM <ismael777xc@informatica.iesvalledeljerteplasencia.es>
2025-10-10 11:26:24 +0200

    d

---
+++ model/Soporte.php
@@ -0,0 +1,47 @@
+<?php 
+
+class Soporte
+{
+    public function __construct(
+        private string $titulo = "",
+        private int $numero = 0,
+        private int $precio = 0,
+    ) {}
+
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    }
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+    public function __toString()
+    {
+        return "Soporte: " . $this->titulo . " - Número: " . $this->numero . " - Precio: " . $this->precio . " €";
+    }
+
+}
+
+
+
+
+?>
