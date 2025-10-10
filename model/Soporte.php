8b8ee9a16897cebe00714c822e0d7a762621a60a
AitorTrujillo9 <atrujillop05@informatica.iesvalledeljerteplasencia.es>
2025-10-10 11:51:06 +0200

    Ejercicio 320 -> Se ha creado la clase inicio.php y se han creado los metodos getPrecioConIva y muestraResumen en Soporte.php

--- model/Soporte.php
+++ model/Soporte.php
@@ -1,38 +1,42 @@
 <?php 
 
-class Soporte
-{
-    const IVA = 0.21;
+class Soporte{
+    private const IVA = 0.21;
     
+//CONSTRUCTOR
     public function __construct(
         private string $titulo = "",
         private int $numero = 0,
-        private int $precio = 0,
+        private float $precio = 0,
     ) {}
 
-    public function getTitulo()
-    {
+//GETTERS
+    public function getTitulo(){
         return $this->titulo;
     }
     
-    public function getNumero()
-    {
+    public function getNumero(){
         return $this->numero;
     }
    
-    public function getPrecio()
-    {
+    public function getPrecio(){
         return $this->precio;
     }
-    
-    public function __toString()
-    {
-        return "Soporte: " . $this->titulo . " - Número: " . $this->numero . " - Precio: " . $this->precio . " €";
+
+//METODOS
+
+    //Metodo que devuelve el precio con el IVA incluido
+    public function getPrecioConIva(){
+        return $this->precio * (1 + self::IVA);
+    }
+
+    //Metodo que muestra un resumen de los datos del soporte
+    public function muestraResumen(){
+        echo
+            '<br>' . $this->titulo . '<br>' . $this->precio . ' (IVA no incluido)'
+        ;
     }
 
 }
-
-
-
 
 ?>
